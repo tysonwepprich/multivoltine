9d3b5eeba4b3552c543e0e7e8a3484ee2d02e2e9
Tyson Wepprich <twepprich@rstudio01.research.sesync.org>
2015-11-04 07:55:08 -0500

    Outputs data in right format in list for model fitting

--- bootstrapM.R
+++ bootstrapM.R
@@ -15,7 +15,7 @@
 # setwd("stopover")
 # devtools::install_github("SESYNC-ci/rslurm")
 
-# library(rslurm)
+library(rslurm)
 library(devtools)
 library(msm)
 library(parallel)
@@ -29,11 +29,11 @@ library(dplyr)
 # for linux/sesync cluster
 # remove.packages('StopoverCode') # do this to rebuild after edits
 # install.packages("StopoverCode", repos = NULL, type="source")
-# library(StopoverCode)
+library(StopoverCode)
 
 # for windows laptop
 # load_all works, not install.package
-devtools::load_all("StopoverCode", recompile = TRUE)
+# devtools::load_all("StopoverCode", recompile = TRUE)
 
 # load data
 count_array <- readRDS('count_array_expanded.rds')
@@ -211,11 +211,275 @@ SlurmCovs <- function(nRun){
 
 # calculate null hypotheses for M = 1:5 for different species
 baseline <- slurm_apply(f = SlurmCovs, params = paramIN, 
-                     cpus_per_node = 8, nodes = 4, 
+                     cpus_per_node = 8, nodes = 6, 
                      data_file = "dataIN.RData", 
                      # pkgs = c("devtools", "msm", "rslurm", "StopoverCode"), 
                      output = "raw")
 
 
 
+# simulation of sample from MLE parameters for each species/M hypothesis
 
+# slurm_out <- get_slurm_out(baseline)
+# saveRDS(slurm_out, "baselineMtest.rds")
+slurm_out <- readRDS("baselineMtest.rds")
+
+outList <- slurm_out
+outDF <- list()
+for (i in 1:length(outList)){
+  out <- outList[[i]]$pars
+  out$model <- i
+  out$ll.val <- outList[[i]]$ll.val
+  if (is.na(out$ll.val)){
+    out$npar <- NA
+  }else{
+    out$npar <- outList[[i]]$npar
+  }
+  out$time <- as.double(outList[[i]]$time, units = "mins")
+  outDF[[i]] <- out
+}
+
+outDF <- do.call("rbind", outDF)
+
+# what to do about ll.val == NA for M = 1?
+nsim <- 100
+SampleList <- vector("list", length = length(slurm_out)*nsim)
+
+for (bs in 1:length(SampleList)){
+  mod <- (bs + nsim - 1) %/% nsim
+  nullFit <- slurm_out[[mod]]
+  # building output list
+  SampleList[[bs]]$pars <- nullFit$pars
+  # move on to next model if ll.val is NA
+  if (is.na(nullFit$ll.val)){
+    SampleList[[bs]]$ll.val <- NA
+    SampleList[[bs]]$npar <- NA
+    next
+  }else{
+    SampleList[[bs]]$ll.val <- nullFit$ll.val
+    SampleList[[bs]]$npar <- nullFit$npar
+  }
+  
+  species <- nullFit$pars$species
+  M <- nullFit$pars$M
+  counts <- count_array[, , species] 
+  
+  # select sites with enough individuals counted
+  siteRows <- which(rowSums(counts, na.rm = TRUE) >= raw_cutoff)
+  counts <- counts[siteRows, ]
+  
+  S <<- dim(counts)[1] 
+  K <<- dim(counts)[2]  
+  TIME <<- dim(counts)[2]
+  
+  N.est <- nullFit$N.est
+  phi.est <- nullFit$phi.est[1,1,1]
+  c0.est <- nullFit$cest[1]
+  c1.est <- nullFit$cest[2]
+  b0.est <- nullFit$b0.est
+  b1.est <- nullFit$b1.est
+  d0.est <- nullFit$d0.est
+  d1.est <- nullFit$d1.est
+  sigma.est <- nullFit$sigma.est
+  w.est <- nullFit$w.est
+  
+  simData <- list()
+    N.tr <- rpois(S, N.est)
+    phi.tr <- matrix(phi.est, TIME-1, TIME-1)
+    
+    cov.p_vary <- array(runif(S*TIME, -1, 1), c(S, TIME))
+    
+    c0.tr <- c0.est
+    c1.tr <- c1.est
+    p_vary.tr <- expo(c0.tr + c1.tr*cov.p_vary)
+    
+    cov.site_all <- rnorm(S)
+    b0.tr <- matrix(rep(b0.est, S), ncol = M, byrow = TRUE)
+    b1.tr <- b1.est
+    mu.tr <- exp(b0.tr + b1.tr*cov.site_all)
+    d0.tr <- d0.est
+    d1.tr <- d1.est
+    sd.tr <- sigma.est
+    
+    if (ncol(w.est) == 1){
+      w.tr <- w.est
+    }else{
+      w.tr <- matrix(NA, ncol = M, nrow = S)
+      for(site in 1:S){
+        w.tr[site,] <- lgp(d0.tr + d1.tr * cov.site_all[site]) 
+      }    
+    }
+    
+    betta.tr <- matrix(0, S, TIME)
+    if (M == 1){
+      for(s in 1:S){
+        betta.tr[s,] <-  c(pnorm(1, mean=mu.tr[s], sd=sd.tr[s]),
+                           pnorm(2:(TIME-1),mean=mu.tr[s],sd=sd.tr[s])-pnorm(1:(TIME-2), mean=mu.tr[s], sd=sd.tr[s]),
+                           1-pnorm(TIME-1,mean=mu.tr[s],sd=sd.tr[s]))
+      }
+    }else{
+      for(s in 1:S){
+        betta.site <- rep(0,TIME)
+        for(m in 1:M){
+          betta.site <- betta.site + c(w.tr[s,m]*c(pnorm(1,mean=mu.tr[s,m],sd=sd.tr[s,m]),
+                                                   pnorm(2:(TIME-1), mean=mu.tr[s,m], sd=sd.tr[s,m])-pnorm(1:(TIME-2),
+                                                                                                           mean=mu.tr[s,m],sd=sd.tr[s,m]), 1-pnorm(TIME-1,mean=mu.tr[s,m],sd=sd.tr[s,m])))
+        }
+        betta.tr[s,] <- betta.site
+      }
+    }
+    
+    
+    
+    lambda.tr <- array(0, c(S, TIME))
+    counts_all <- array(0,c(S, TIME))
+    
+    for(i in 1:S){
+      
+      lambda.tr[i,] <- N.tr[i]*betta.tr[i,]
+      counts_all[i,] <- rpois(TIME, lambda.tr[i,]*p_vary.tr[i,])	
+      
+      for(j in 2:TIME){
+        for(b in 1:(j-1)){
+          lambda.tr[i,j] <- lambda.tr[i,j] + N.tr[i]*betta.tr[i,b]*prod(phi.tr[b,b:(j-1)])
+        }
+        counts_all[i,j] <- rpois(1,lambda.tr[i,j]*p_vary.tr[i,j])
+      }
+    }
+    #drop records to approximate rate of missing survey each week
+    
+    counts_missing <- array(0, c(S, TIME))
+    
+    PropSurv <- function(vec){
+      prop <- length(which(is.na(vec) == FALSE))/length(vec)
+      return(prop)
+    }
+    FreqSurv <- apply(counts, 2, PropSurv)
+    for (s in 1:S){
+      counts_missing[s,] <- rbinom(TIME, 1, FreqSurv)
+    }
+    counts_missing[counts_missing == 0] <- NA
+    counts_missing <- counts_all * counts_missing
+    counts_missing[is.na(counts_missing)] <- -1 #reassign NA for C program
+    
+    
+    simData$counts <- counts_missing
+    simData$p_cov <- cov.p_vary
+    simData$site_cov <- cov.site_all
+
+    SampleList[[bs]]$simData <- simData
+} #close Sample for loop
+
+# saveRDS(SampleList, file = "SampleList.rds")
+SampleList <- readRDS("SampleList.rds")
+
+# fit the bootstrap sample datasets using SLURM
+
+# data_file Rdata
+dataIN <- c("SampleList")
+save(list = dataIN, file = "dataIN.RData")
+
+# simple param file for slurm.apply
+paramIN <- data.frame(nRun = seq(1:length(SampleList)))
+
+SlurmGeneration <- function(nRun){
+  BSdata <- SampleList[[nRun]]
+  # parameters
+  pars <- BSdata$pars
+  species <- pars$species
+  raw_cutoff <- pars$raw_cutoff
+  p_cov1 <- pars$p_cov1
+  p_cov2 <- pars$p_cov2
+  site_covs <- pars$site_covs
+  M <<- pars$M
+  sigma.m <<- pars$sigma.m
+  phi.m <<- pars$phi.m
+  
+  ### data prep ###
+  counts <- count_array[, , species] 
+  
+  # select sites with enough individuals counted
+  siteRows <- which(rowSums(counts, na.rm = TRUE) >= raw_cutoff)
+  counts <- counts[siteRows, ]
+  counts[is.na(counts)] <- -1 #reassign NA for C program
+  ALLcounts <- counts # ALL distinguishes from bootstrapped counts
+  
+  S <<- dim(counts)[1] 
+  K <<- dim(counts)[2]  
+  TIME <<- dim(counts)[2]
+  
+  # covariates
+  # detection probability
+  covs <- c(p_cov1, p_cov2)
+  covs <- sort(as.numeric(covs[covs %in% as.character(c(1:7))]))
+  if (length(covs) == 0) {
+    p.m <<- "common" 
+    cov.p <- NA
+  } else {
+    p.m <<- "cov"
+    if (length(covs) > 1) cov.p <- cov_array[, , covs]
+    if (length(covs) == 1) cov.p <- array(data = cov_array[, , covs], dim = c(dim(cov_array[,,covs]), 1))
+    cov.p <- cov.p[siteRows, , , drop = FALSE]
+    # qp is the number of covariates for p
+    qp <- dim(cov.p)[3]
+    for(q in 1:qp) cov.p[,,q] <- scale(cov.p[,,q])[1:S,1:K]
+    cov.p[is.na(cov.p)] <- -1
+  }
+  ALLcov.p <- cov.p
+  
+  
+  # Time (week) covariate for phi
+  cov.phi <-  matrix(((1:(K-1))-mean(1:(K-1)))/sqrt(var(1:(K-1))), S, K-1, byrow=TRUE) 
+  ALLcov.phi <- cov.phi
+  
+  # Site covariates for mu and weight of broods
+  if (site_covs == "common") {
+    s_cov <- NA
+    mu.m <<- "common"
+    w.m <<- "common"
+  }else{
+    mu.m <<- "cov"
+    w.m <<- "cov"
+    if (site_covs == "AnnGDD") s_cov <- scale(cov_sites$YearGDD)
+    if (site_covs == "SprGDD") s_cov <- scale(cov_sites$SpringGDD)
+    if (site_covs == "lat") s_cov <- scale(cov_sites$lat)
+    s_cov <- s_cov[siteRows]
+  }
+  ALLcov.w <- s_cov
+  ALLcov.mu <- s_cov
+  
+  startTime <- Sys.time()
+  
+  counts <<- ALLcounts
+  cov.w <<- ALLcov.w
+  cov.mu <<- ALLcov.mu
+  cov.p <<- ALLcov.p
+  cov.phi <<- ALLcov.phi
+  qp <<- dim(cov.p)[3]
+  
+  ####
+  out <- list()
+  Tries <- 5
+  temp.fit <- list()
+  temp.ll <- rep(NA, Tries)
+  
+  for (k in 1:Tries){
+    start.list <<- startVals(p.m,w.m,mu.m,sigma.m,phi.m)
+    pars.start <<- c(start.list$N,start.list$cvec, start.list$d0, start.list$d1, start.list$b0, start.list$b1,start.list$sigma,  start.list$a0, start.list$a1, start.list$a2) #this line remains the same for all models
+    temp.fit[[k]] <- mLLMixtCounts.fit(p.m,w.m,mu.m,sigma.m,phi.m)
+    temp.ll[k] <- temp.fit[[k]]$ll.val
+  }
+  
+  if (length(which(is.na(temp.ll) == TRUE)) < Tries){
+    tempchoose <- min(c(1:Tries)[which(temp.ll==max(temp.ll, na.rm=TRUE))])
+    temp <-temp.fit[[tempchoose]]
+  }else{
+    temp <- list()
+    temp$ll.val <- NA
+  }
+  
+  temp$time <- startTime - Sys.time()
+  temp$pars <- pars
+  out[[1]] <- temp
+  return(out)
+}
