60f77a6ac3bffef65db84f6ce4744682f5838aa3
Tyson Wepprich <tyson.wepprich@gmail.com>
2016-04-07 15:22:55 -0400

    changed bash script to try ncsu hpc again

--- hpc_mboot_debug.sh
+++ hpc_mboot_debug.sh
@@ -1,7 +1,9 @@
 #!/bin/tcsh
 #BSUB -n 4
-#BUSB -W 15
+#BSUB -R span[hosts=1]
+#BSUB -W 15
+setenv OMP_NUM_THREADS 4
 source /usr/local/apps/R/R-2.15-parallel.csh
-R CMD BATCH --vanilla  hpc_mbootstrap.r 
+R CMD BATCH --vanilla  hpc_mbootstrap.R 
 #BSUB -o out.%J
 #BSUB -e err.%J
